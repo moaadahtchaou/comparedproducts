313bd4f4c6f0242133768a2259b364127ecc8b1d
moaadahtchaou <moaadahtchao@gmail.com>
2025-06-01 22:44:56 +0100

    code3

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import {
   Card,
   CardContent,
